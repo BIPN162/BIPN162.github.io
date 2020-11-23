f166e640c7270939c0403167afb61b636db4c76b
Victor Magdaleno <VictorMagdaleno@MacBook-Pro.attlocal.net>
2020-11-23 09:13:31 -0800

    adding my first book

---
+++ _build/jupyter_execute/landing_page.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# #                            Welcome to NWB 101
+
+# ## *A Hands-On Guide to Neural Data Science*
+# 
+
+# 
+# ```{toctree}
+# :hidden:
+# :titlesonly:
+# 
+# 
+# Data_Science_In_Python
+# Neurodata_Without_Borders
+# The_Allen_SDK
+# Single-Cell_Electrophysiology
+# Connectivity_Atlas
+# ```
+# 
